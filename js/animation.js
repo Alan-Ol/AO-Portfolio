32e35160c60bbdd38d3c1fcde507424e9c1f2270
Alan.o <olmedoalan821@gmail.com>
2022-10-15 18:39:29 -0300

    nuevo-portfolio

---
+++ js/animation.js
@@ -0,0 +1,50 @@
+
+
+window.sr = ScrollReveal({reset: true});
+
+    sr.reveal('.home', {
+        duration: 2000,
+        origin: "left",
+        distance: "70px"
+    });
+    sr.reveal('.home-img', {
+        duration: 2000,
+        origin: "bottom",
+        distance: "70px"
+    });
+    sr.reveal('.about-img', {
+        duration: 2000,
+        origin: "left",
+        distance: "70px"
+    });
+    sr.reveal('.about-text', {
+        duration: 2000,
+        origin: "left",
+        distance: "70px"
+    });
+    sr.reveal('.services', {
+        duration: 2000,
+        origin: "bottom",
+        distance: "70px"
+    });
+    sr.reveal('.card-main', {
+        duration: 2000,
+        origin: "left",
+        distance: "70px"
+    });
+    sr.reveal('.container', {
+        duration: 2000,
+        origin: "left",
+        distance: "70px"
+    });
+    sr.reveal('.container-form', {
+        duration: 2000,
+        origin: "bottom",
+        distance: "70px"
+    });
+    sr.reveal('.container-contact', {
+        duration: 2000,
+        origin: "bottom",
+        distance: "70px"
+    });
+
